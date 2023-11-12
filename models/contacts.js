3a14c404fdcca0aeb5955b37efbf7c15e8b37976
StanislavBobrivskyi <121600218+StanislavBobrivskyi@users.noreply.github.com>
2023-11-12 09:09:40 +0100

    working with connection for data base

--- models/contacts.js
+++ models/contacts.js
@@ -1,91 +1,189 @@
-const fs = require("node:fs/promises");
+// const fs = require("node:fs/promises");
 
-const path = require("node:path");
+// const path = require("node:path");
 
-const crypto = require("node:crypto");
+// const crypto = require("node:crypto");
 
-const contactsPath = path.join(__dirname, "contacts.json");
+// const contactsPath = path.join(__dirname, "contacts.json");
 
-async function readContacts() {
-  try {
-    const data = await fs.readFile(contactsPath, { encoding: "UTF-8" });
-    return JSON.parse(data);
-  } catch (error) {
-    return [];
-  }
-}
+// async function readContacts() {
+//   try {
+//     const data = await fs.readFile(contactsPath, { encoding: "UTF-8" });
+//     return JSON.parse(data);
+//   } catch (error) {
+//     return [];
+//   }
+// }
 
-function writeContacts(contacts) {
-  return fs.writeFile(contactsPath, JSON.stringify(contacts, undefined, 2));
-}
+// function writeContacts(contacts) {
+//   return fs.writeFile(contactsPath, JSON.stringify(contacts, undefined, 2));
+// }
+
+// const listContacts = async () => {
+//   const contactsList = await readContacts();
+
+//   return contactsList;
+// };
+
+// const getContactById = async (contactId) => {
+//   const contactsList = await readContacts();
+//   const contact =
+//     contactsList.find((contact) => contact.id === contactId) || null;
+
+//   return contact;
+// };
+
+// const removeContact = async (contactId) => {
+//   const contactsList = await readContacts();
+//   const index = contactsList.findIndex((contact) => contact.id === contactId);
+
+//   if (index === -1) {
+//     return null;
+//   }
+
+//   const deleteContact = [
+//     contactsList.slice(0, index),
+//     ...contactsList.slice(index + 1),
+//   ];
+//   await writeContacts(deleteContact);
+
+//   return contactsList[index];
+// };
+
+// const addContact = async (name, email, phone) => {
+//   const contactsList = await readContacts();
+//   const newContact = {
+//     id: crypto.randomUUID(),
+//     name: name,
+//     email: email,
+//     phone: phone,
+//   };
+//   contactsList.push(newContact);
+
+//   await writeContacts(contactsList);
+//   return newContact;
+// };
+
+// const updateContact = async (contactId, name, email, phone) => {
+//   const contactsList = await readContacts();
+//   const index = contactsList.findIndex((contact) => contact.id === contactId);
+
+//   if (index === -1) {
+//     return null;
+//   }
+//   contactsList[index] = {
+//     id: contactId,
+//     name: name,
+//     email: email,
+//     phone: phone,
+//   };
+
+//   await writeContacts(contactsList);
+//   return contactsList[index];
+// };
+
+// module.exports = {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+//   updateContact,
+// };
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/contacts-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.error("Database connection error:", error);
+  process.exit(1);
+});
+
+db.once("open", () => {
+  console.log("Database connection successful");
+});
+
+// Оголосіть схему моделі
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: String,
+  phone: String,
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+// Створіть модель зі схемою
+const Contact = mongoose.model("Contact", contactSchema);
+
+// Оновлені функції обробки запитів
 
 const listContacts = async () => {
-  const contactsList = await readContacts();
-
-  return contactsList;
+  try {
+    const contacts = await Contact.find();
+    console.log(contacts);
+    return contacts;
+  } catch (error) {
+    console.error("Error listing contacts:", error);
+  }
 };
 
 const getContactById = async (contactId) => {
-  const contactsList = await readContacts();
-  const contact =
-    contactsList.find((contact) => contact.id === contactId) || null;
+  try {
+    const contact = await Contact.findById(contactId);
+    console.log(contact);
+    return contact;
+  } catch (error) {
+    console.error("Error getting contact by ID:", error);
+  }
+};
 
-  return contact;
+const addContact = async (newContact) => {
+  try {
+    const contact = new Contact(newContact);
+    const savedContact = await contact.save();
+    console.log(savedContact);
+    return savedContact;
+  } catch (error) {
+    console.error("Error adding contact:", error);
+  }
+};
+
+const updateContact = async (contactId, updatedContact) => {
+  try {
+    const contact = await Contact.findByIdAndUpdate(contactId, updatedContact, {
+      new: true,
+    });
+    console.log(contact);
+    return contact;
+  } catch (error) {
+    console.error("Error updating contact:", error);
+  }
 };
 
 const removeContact = async (contactId) => {
-  const contactsList = await readContacts();
-  const index = contactsList.findIndex((contact) => contact.id === contactId);
-
-  if (index === -1) {
-    return null;
+  try {
+    const contact = await Contact.findByIdAndRemove(contactId);
+    console.log("Contact removed:", contact);
+    return contact;
+  } catch (error) {
+    console.error("Error removing contact:", error);
   }
-
-  const deleteContact = [
-    contactsList.slice(0, index),
-    ...contactsList.slice(index + 1),
-  ];
-  await writeContacts(deleteContact);
-
-  return contactsList[index];
-};
-
-const addContact = async (name, email, phone) => {
-  const contactsList = await readContacts();
-  const newContact = {
-    id: crypto.randomUUID(),
-    name: name,
-    email: email,
-    phone: phone,
-  };
-  contactsList.push(newContact);
-
-  await writeContacts(contactsList);
-  return newContact;
-};
-
-const updateContact = async (contactId, name, email, phone) => {
-  const contactsList = await readContacts();
-  const index = contactsList.findIndex((contact) => contact.id === contactId);
-
-  if (index === -1) {
-    return null;
-  }
-  contactsList[index] = {
-    id: contactId,
-    name: name,
-    email: email,
-    phone: phone,
-  };
-
-  await writeContacts(contactsList);
-  return contactsList[index];
 };
 
 module.exports = {
   listContacts,
   getContactById,
-  removeContact,
   addContact,
   updateContact,
+  removeContact,
 };
--- server.js
+++ server.js
@@ -1,15 +1,26 @@
-const app = require("./app");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://BobrivskyiStanislav:<password>@cluster0.splryg6.mongodb.net/?retryWrites=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+const mongoose = require("mongoose");
+
+const dbUrl =
+  "mongodb+srv://BobrivskyiStanislav:bober237BoBer93012716511@cluster0.splryg6.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.error("Database connection error:", error);
+  process.exit(1);
+});
+
+db.once("open", () => {
+  console.log("Database connection successful");
+});
+
+// Решта коду для схеми, моделі та функцій обробки запитів залишається незмінним
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
